e3aa27e691cfcc4a7572ba8d139db76f8fe025c9
Raghavi Ramamoorthy <95253831+rramamoo@users.noreply.github.com>
2022-09-16 15:55:19 -0500

    Update testfile.ts

--- testfile.ts
+++ testfile.ts
@@ -1,3 +1,8 @@
 console.log("sdf");
+///merge options
 console.log("sddfsfsdff");
 console.log("sddfsfsdff");
+console.log("sddfsfsdff");
+console.log("sddfsfsdff");
+console.log("sdf");
+console.log("sdf");
