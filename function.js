c418ba03f5bb45d2b704302cabd8b94f760bfcc9
dsana16 <dsouzaanagha16@gmail.com>
2022-09-16 21:18:01 -0400

    tsconfig and function files

---
+++ function.js
@@ -0,0 +1,17 @@
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(4, 5));
+var concat = function (x, y) {
+    return x + y;
+};
+console.log(concat("4", "5"));
+/*function multiply (a: number, b: number) {
+    return a * b ;
+}
+
+let concat = function(a: string, b: string){
+    return a + b;
+}
+console.log(multiply(5,5));
+console.log(concat("hello", "JS"));*/ 
---
+++ function.ts
@@ -0,0 +1,19 @@
+function multiply( a: number  , b: number){
+    return a * b;
+}
+console.log(multiply( 4, 5));
+
+var concat = function( x : string , y :string){
+    return x+y;
+}
+console.log(concat( "4" , "5"));
+
+/*function multiply (a: number, b: number) { 
+    return a * b ;
+}
+
+let concat = function(a: string, b: string){
+    return a + b;
+}
+console.log(multiply(5,5));
+console.log(concat("hello", "JS"));*/
